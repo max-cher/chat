a433255736d33fd5ab35c2a7e6347c013e0e420c
max-cher <z3r0krank@gmail.com>
2020-11-15 22:33:33 +0300

    i don't know how selectors works

--- client.py
+++ client.py
@@ -1,22 +1,30 @@
 import socket
 
 
-host = input('host (press <enter> for localhost)>> ')
-if '' == host:
-    host = '127.0.0.1'
+def print_msg(msg):
+    for byte in msg:
+        print(chr(byte), end='')
 
-port = input('port (press <enter> for 6667)>> ')
 
-if '' == port:
-	port = 6667
-else:
-	port = int(port)
+#host = input('host (press <enter> for localhost)>> ')
+#if '' == host:
+#    host = '127.0.0.1'
+#port = input('port (press <enter> for 6667)>> ')
+#if '' == port:
+#	port = 6667
+#else:
+#	port = int(port)
+host, port = '127.0.0.1', 6667
 
-msg = input('msg>> ')
+
+msg = ''
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.connect((host, port))
-    sock.send(msg.encode())
-    #data = s.recv(1024)
+    while(msg != 'exit'):
+        msg = input('msg>> ')
+        #print_msg(msg.encode())
+        sock.send(msg.encode())
+        #data = s.recv(1024)
 
 #print("Received", repr(data))
--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import socket
 import selectors
 import types
 
-sel = selectors.DefaultSelector()
+
 
 
 def accept_wrapper(sock):
@@ -19,24 +19,30 @@ def service_connection(key, mask):
     sock = key.fileobj
     data = key.data
     if mask & selectors.EVENT_READ:
+        #print('EVENT_READ')
         recv_data = sock.recv(1024)  # Should be ready to read
         if recv_data:
+            ##print(type(recv_data))
+            ##print_msg(recv_data)
             data.outb += recv_data
-        else:
-            print("closing connection to", data.addr)
-            sel.unregister(sock)
-            sock.close()
+        #else:
+            #print("closing connection to", data.addr)
+            #sel.unregister(sock)
+            #sock.close()
     if mask & selectors.EVENT_WRITE:
+        #print('EVENT_WRITE')
         if data.outb:
+            print(repr(data.outb))
             #print("echoing", repr(data.outb), "to", data.addr)
-            print_msg(data.outb)
-            sent = sock.send(data.outb)  # Should be ready to write
-            data.outb = data.outb[sent:]
+            #print_msg(data.outb)
+            #sent = sock.send(data.outb)  # Should be ready to write
+            #data.outb = data.outb[sent:]
+            data.outb = ''.encode()#None
 
 
 def print_msg(msg):
     for byte in msg:
-        print(ord(byte))
+        print(chr(byte), end='')
     
 
 
@@ -46,14 +52,15 @@ def print_msg(msg):
 #host, port = sys.argv[1], int(sys.argv[2])
 #print(type(input()))
 
-host = input('host (press <enter> to listen all hosts)>> ')
-port = input('port (press <enter> for 6667)>> ')
+#host = input('host (press <enter> to listen all hosts)>> ')
+#port = input('port (press <enter> for 6667)>> ')
+#if '' == port:
+#	port = 6667
+#else:
+#	port = int(port)
+host, port = '', 6667
 
-if '' == port:
-	port = 6667
-else:
-	port = int(port)
-
+sel = selectors.DefaultSelector()
 
 lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 lsock.bind((host, port))
@@ -62,18 +69,20 @@ print("listening on", (host, port))
 lsock.setblocking(False)
 sel.register(lsock, selectors.EVENT_READ, data=None)
 
-try:
-    while True:
-        events = sel.select(timeout=None)
-        for key, mask in events:
-            if key.data is None:
-                accept_wrapper(key.fileobj)
-            else:
-                service_connection(key, mask)
-except KeyboardInterrupt:
-    print("caught keyboard interrupt, exiting")
-except:
-	input('END/n')
-finally:
-    sel.close()
+#try:
+while True:
+    events = sel.select(timeout=None)
+    for key, mask in events:
+        if key.data is None:
+            #print('accept_wrapper()')
+            accept_wrapper(key.fileobj)
+        else:
+            #print('service_connection()')
+            service_connection(key, mask)
+#except KeyboardInterrupt:
+#    print("caught keyboard interrupt, exiting")
+#except:
+#	input('END/n')
+#finally:
+#    sel.close()
 
