da49ac8cbe2c69514054f77b1516f8704f3e94be
max-cher <z3r0krank@gmail.com>
2020-11-16 04:15:29 +0300

    add 'client' class in server.py

--- client.py
+++ client.py
@@ -1,30 +1,46 @@
 import socket
 
 
+keywords = {'exit' : ['/exit', '/quit', '/leave', '/q'],
+            'me' : ['/me'],
+            }
+
 def print_msg(msg):
     for byte in msg:
         print(chr(byte), end='')
 
+def get_host():
+    #host = input('host (press <enter> for localhost)>> ')
+    #if '' == host:
+    #    host = '127.0.0.1'
+    #port = input('port (press <enter> for 6667)>> ')
+    #if '' == port:
+    #    port = 6667
+    #else:
+    #    port = int(port)
+    host, port = '127.0.0.1', 6667  # debug mode!
+    return host, port
 
-#host = input('host (press <enter> for localhost)>> ')
-#if '' == host:
-#    host = '127.0.0.1'
-#port = input('port (press <enter> for 6667)>> ')
-#if '' == port:
-#	port = 6667
-#else:
-#	port = int(port)
-host, port = '127.0.0.1', 6667
 
+host, port = get_host()
+
+print('CLIENT')
 
 msg = ''
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    print('connecting to {} port {}'.format(host, port))
     sock.connect((host, port))
-    while(msg != 'exit'):
+    sock.setblocking(False)
+    while(True):
         msg = input('msg>> ')
-        #print_msg(msg.encode())
-        sock.send(msg.encode())
+        if msg not in keywords['exit']:
+            sock.send(msg.encode())
+        elif msg in keywords['exit']:
+            print('closeing connection')
+            sock.close()
+            break
         #data = s.recv(1024)
+    input('press <Enter>')
 
 #print("Received", repr(data))
--- server.py
+++ server.py
@@ -3,12 +3,20 @@ import socket
 import selectors
 import types
 
-
+keywords = ['/exit',
+            '/nick',
+            '/me',
+            '/who',
+            '/help',
+            ]
 
 
 def accept_wrapper(sock):
+    '#Callback for new connections'
     conn, addr = sock.accept()  # Should be ready to read
-    print("accepted connection from", addr)
+    print('#accepted connection from', addr)
+    #clients.append(client(sock))
+    clients[addr[1]] = client(sock, int(addr[1]))
     conn.setblocking(False)
     data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
     events = selectors.EVENT_READ | selectors.EVENT_WRITE
@@ -18,56 +26,124 @@ def accept_wrapper(sock):
 def service_connection(key, mask):
     sock = key.fileobj
     data = key.data
+    
     if mask & selectors.EVENT_READ:
-        #print('EVENT_READ')
         recv_data = sock.recv(1024)  # Should be ready to read
         if recv_data:
-            ##print(type(recv_data))
-            ##print_msg(recv_data)
             data.outb += recv_data
-        #else:
-            #print("closing connection to", data.addr)
-            #sel.unregister(sock)
-            #sock.close()
+        else:
+            clients.pop(data.addr[1])
+            #print(sock)
+            sel.unregister(sock)
+            sock.close()
+            
     if mask & selectors.EVENT_WRITE:
-        #print('EVENT_WRITE')
         if data.outb:
-            print(repr(data.outb))
-            #print("echoing", repr(data.outb), "to", data.addr)
-            #print_msg(data.outb)
-            #sent = sock.send(data.outb)  # Should be ready to write
-            #data.outb = data.outb[sent:]
+            
+            msg_from = data.outb.decode('utf-8')
+            if len(msg_from.split()) > 1:
+                keyword  = msg_from.split()[0]
+                msg_tail = msg_from.replace(keyword + ' ', '')
+            else:
+                keyword  = msg_from
+                msg_tail = ''
+            client_from = clients[sock.getpeername()[1]]
+            #print('keyword: [', type(keyword), '] : [', keyword , '] len = ', len(keyword), sep='')
+            
+            
+            if keyword not in keywords:
+                send_all(client_from.nick + ': ' + msg_from)
+                #if(len(clients) > 1):
+                #    sent = sock.send(data.outb)  # Should be ready to write
+                #data.outb = data.outb[sent:]
+            elif keyword == '/nick':
+                client_from.set_nick(msg_tail)
+            elif keyword == '/who':
+                for cl in clients:
+                    client_from.send_msg(clients[cl].nick)
+            elif keyword == '/me':
+                send_all('#' + client_from.nick + ' ' + msg_tail)
+            else:
+                print('en tieda...')
+                
             data.outb = ''.encode()#None
 
+def send_all(msg):
+    for client_id in clients:
+        clients[client_id].send_msg(msg)
 
-def print_msg(msg):
-    for byte in msg:
-        print(chr(byte), end='')
+class client():
     
+    counter = 0
+    
+    def __init__(self, sock, id):
+        self.sock = sock
+        self.id = id
+        client.counter += 1
+        self.nick = 'client_' + str(self.id)
+        self.send_msg('Welcome to server! your nick is{}.\nType "/nick <new nick>" to change it'.format(self.nick))
+        send_all('#' + self.nick + ' connected')
+        client.print_clients()
+    
+    def __del__(self):
+        client.counter -= 1
+        print('#closing connection to', self.nick)
+        #sel.unregister(self.sock)
+        #self.sock.close()
+        #print(self.sock)
+        client.print_clients()
+    
+    def set_nick(self, nick):
+        print('#now {} is {}'.format(self.nick, nick))
+        self.nick = nick
+    
+    def send_msg(self, msg):
+        '''
+        msg can be bytes or str
+        '''
+        if type(msg) == type('a'.encode()):
+            msg =  msg.decode('utf-8')
+        print('sending to: {}, msg: {}'.format(self.nick, msg))
+        #self.sock.send(msg)
+        pass
+    
+    def print_clients():
+        print('Total clients connected: {}'.format(client.counter))
 
 
-#if len(sys.argv) != 3:
-#    print("usage:", sys.argv[0], "<host> <port>")
-#    sys.exit(1)
-#host, port = sys.argv[1], int(sys.argv[2])
-#print(type(input()))
 
-#host = input('host (press <enter> to listen all hosts)>> ')
-#port = input('port (press <enter> for 6667)>> ')
-#if '' == port:
-#	port = 6667
-#else:
-#	port = int(port)
-host, port = '', 6667
+
+def get_host():
+    '''
+    get host, port from keyboard
+    '''
+    #host, port = sys.argv[1], int(sys.argv[2])
+    #print(type(input()))
+    
+    #host = input('host (press <enter> to listen all hosts)>> ')
+    #port = input('port (press <enter> for 6667)>> ')
+    #if '' == port:
+    #	port = 6667
+    #else:
+    #	port = int(port)
+    host, port =  '', 6667 # debug mode
+    return host, port
+
+host, port = get_host()
 
 sel = selectors.DefaultSelector()
+
+print('SERVER')
 
 lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 lsock.bind((host, port))
 lsock.listen()
-print("listening on", (host, port))
+print('listening on', (host, port))
 lsock.setblocking(False)
+
 sel.register(lsock, selectors.EVENT_READ, data=None)
+
+clients = {}
 
 #try:
 while True:
@@ -85,4 +161,5 @@ while True:
 #	input('END/n')
 #finally:
 #    sel.close()
+sel.close()
 
