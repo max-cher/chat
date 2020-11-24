b2eb32574dca48e0f9111319cb3fe319e49cb5d1
Maxim Cherepanov <m.cherepanov@dokltd.ru>
2020-11-24 17:57:20 +0300

    oop refactored

--- client.py
+++ client.py
@@ -1,11 +1,6 @@
 import socket
 from multiprocessing import Queue
-import sys
 import threading
-import configparser
-import os
-
-
 
 
 keywords = {'exit' : ['/exit', '/quit', '/leave', '/q'],
@@ -13,79 +8,55 @@ keywords = {'exit' : ['/exit', '/quit', '/leave', '/q'],
             }
 
 
-
-def get_input():
-    global run_flag
-    #for line in iter(input, 0):#sys.stdin.readline, ''):
-    while run_flag:
-        q_send.put(input(''))
-    sys.stdin.close()
-
-
-def send_msg():
-    global run_flag
-    while run_flag:
-        msg = q_send.get().replace('\n', '')
-        if msg not in keywords['exit']:
-            sock.send(msg.encode())
-        elif msg in keywords['exit']:
-            sock.send('Press <Enter>'.encode()) # dirty hack to unblock rcv_msg and then close the connection
-            run_flag = False
-
-
-def rcv_msg():
-    global run_flag
-    while run_flag:
-        data = sock.recv(1024)
-        if(data):
-            print(data.decode('utf-8'))
-
-
-def get_host():
-    #config = configparser.ConfigParser()
-    #if os.path.exists('config.ini'):          
-    #    config.read('config.ini')
-    #    #print(config.sections())
-    #    host = config['DEFAULT']['host']
-    #    port = int(config['DEFAULT']['port'])
+class Client():
     
-    host, port = '192.168.127.254', 23  # debug mode!
-    host1 = input('host (press <enter> for {})>> '.format(host))
-    if '' != host1:
-        host = host1
+    def __init__(self):
+        print('=CLIENT=')
+        self.set_host()
+        self.run_flag = True
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as self.sock:
+            print('Connecting to {} port {}'.format(self.host, self.port))
+            self.sock.connect((self.host, self.port))
+            self.sender = threading.Thread(name='sender', target=self.send_msg).start()
+            
+            while(self.run_flag):
+                self.data = self.sock.recv(1024)
+                if(self.data):
+                    print(self.data.decode('utf-8'))
+
+    def set_host(self):
+        self.host, self.port = '192.168.127.254', 23  # default
+        host = input('Host (press <Enter> for {})>> '.format(self.host))
+        if '' != host:
+            self.host = host
+        
+        while True:
+            port = input('Port (press <Enter> for {})>> '.format(self.port))
+            if '' != port:
+                try:
+                    self.port = int(port)
+                    break
+                except:
+                    print('Port must be num type!')
+            break
+
+    def send_msg(self):
+        while self.run_flag:
+            self.msg = input('').replace('\n', '')
+            if self.msg not in keywords['exit']:
+                self.sock.send(self.msg.encode())
+            elif self.msg in keywords['exit']:
+                self.sock.send(' '.encode()) # dirty hack to unblock rcv_msg and then close the connection
+                self.run_flag = False
     
-    port1 = input('port (press <enter> for {})>> '.format(port))
-    if '' != port1:
-        port = int(port1)
-    
-    return host, port
+    def __del__(self):
+        print('Disconnecting')
 
 
 
-
-print('CLIENT')
-host, port = get_host()
-run_flag = True
-msg = ''
-
-q_send = Queue()
-q_recv = Queue()
+if __name__ == '__main__':
+    Client()
 
 
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    #sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    print('connecting to {} port {}'.format(host, port))
-    sock.connect((host, port))
-    #sock.setblocking(False)
-    
-    
-    
-    input_getter = threading.Thread(name='input-getter', target=get_input).start()
-    sender = threading.Thread(name='sender', target=send_msg).start()
-    receiver = threading.Thread(name='receiver', target=rcv_msg).start()
-    
-    
-    while(run_flag):
-        pass
 
